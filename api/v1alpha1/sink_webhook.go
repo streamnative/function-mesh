d9f68ec1c1e79845b098a008996aeb5cef52340a
xiaolong.ran <rxl@apache.org>
2020-11-30 12:04:52 +0800

    fix panic issue for function mesh
    Signed-off-by: xiaolong.ran <rxl@apache.org>

--- api/v1alpha1/sink_webhook.go
+++ api/v1alpha1/sink_webhook.go
@@ -51,6 +51,11 @@ func (r *Sink) Default() {
 		r.Spec.Replicas = &zeroVal
 	}
 
+	if r.Spec.MaxReplicas == nil {
+		zeroVal := int32(0)
+		r.Spec.MaxReplicas = &zeroVal
+	}
+
 	if r.Spec.AutoAck == nil {
 		trueVal := true
 		r.Spec.AutoAck = &trueVal
@@ -72,12 +77,14 @@ func (r *Sink) Default() {
 		r.Spec.Tenant = DefaultTenant
 	}
 
-	if r.Spec.Resources.Requests.Cpu() == nil {
-		r.Spec.Resources.Requests.Cpu().Set(int64(1))
-	}
+	if r.Spec.Resources.Requests != nil {
+		if r.Spec.Resources.Requests.Cpu() == nil {
+			r.Spec.Resources.Requests.Cpu().Set(int64(1))
+		}
 
-	if r.Spec.Resources.Requests.Memory() == nil {
-		r.Spec.Resources.Requests.Memory().Set(int64(1073741824))
+		if r.Spec.Resources.Requests.Memory() == nil {
+			r.Spec.Resources.Requests.Memory().Set(int64(1073741824))
+		}
 	}
 
 	if r.Spec.Resources.Limits == nil {
@@ -107,7 +114,7 @@ func (r *Sink) ValidateCreate() error {
 		return errors.New("replicas cannot be zero")
 	}
 
-	if *r.Spec.MaxReplicas != 0 && *r.Spec.Replicas > *r.Spec.MaxReplicas {
+	if r.Spec.MaxReplicas != nil && *r.Spec.Replicas > *r.Spec.MaxReplicas {
 		return errors.New("maxReplicas must be greater than or equal to replicas")
 	}
 
