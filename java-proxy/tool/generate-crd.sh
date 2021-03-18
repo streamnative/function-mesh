023f7d8de7efc547d504f42216843656adcd7fbd
guangning <guangning@apache.org>
2021-03-18 21:36:10 +0800

    Delete it args

--- java-proxy/tool/generate-crd.sh
+++ java-proxy/tool/generate-crd.sh
@@ -43,7 +43,6 @@ docker run \
   --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$(pwd)":"$(pwd)" \
-  -ti \
   --network host \
   docker.pkg.github.com/kubernetes-client/java/crd-model-gen:v1.0.3 \
   /generate.sh \
@@ -57,7 +56,6 @@ docker run \
   --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$(pwd)":"$(pwd)" \
-  -ti \
   --network host \
   docker.pkg.github.com/kubernetes-client/java/crd-model-gen:v1.0.3 \
   /generate.sh \
@@ -71,7 +69,6 @@ docker run \
   --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$(pwd)":"$(pwd)" \
-  -ti \
   --network host \
   docker.pkg.github.com/kubernetes-client/java/crd-model-gen:v1.0.3 \
   /generate.sh \
