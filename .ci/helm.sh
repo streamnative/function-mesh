43edf3037e1b1ca749641fd00648f351afe2d4f9
Rui Fu <freelz@gmail.com>
2021-08-05 14:20:09 +0800

    add delete test

--- .ci/helm.sh
+++ .ci/helm.sh
@@ -268,6 +268,17 @@ function ci::verify_mesh_worker_service_pulsar_admin() {
     WC=$(${KUBECTL} get pods -n ${NAMESPACE} --field-selector=status.phase=Running | grep "data-generator-source" | wc -l)
   done
   ${KUBECTL} exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin sinks status --name data-generator-sink
+  RET=$(${KUBECTL} exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin sinks status --name data-generator-sink)
+  if [[ $RET != *"true"* ]]; then
+   return 1
+  fi
   ${KUBECTL} exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin sources status --name data-generator-source
+  RET=$(${KUBECTL} exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin sources status --name data-generator-source)
+  if [[ $RET != *"true"* ]]; then
+   return 1
+  fi
+  ${KUBECTL} get pods -n ${NAMESPACE}
+  ${KUBECTL} exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin sources delete --name data-generator-source
+  ${KUBECTL} exec -n ${NAMESPACE} ${CLUSTER}-pulsar-broker-0 -- bin/pulsar-admin sinks delete --name data-generator-sink
   ${KUBECTL} get pods -n ${NAMESPACE}
 }
