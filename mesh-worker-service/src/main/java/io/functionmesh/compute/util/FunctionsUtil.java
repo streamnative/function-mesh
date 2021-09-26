3c6e16e4c6cb539ca73c6080c62efbd525d9137f
Rui Fu <freeznet@users.noreply.github.com>
2021-09-26 08:53:14 +0800

    fix mesh worker service class loader and cleanup temp file (#282)
    * fix mesh worker service type extract classloader
    
    * cleanup

--- mesh-worker-service/src/main/java/io/functionmesh/compute/util/FunctionsUtil.java
+++ mesh-worker-service/src/main/java/io/functionmesh/compute/util/FunctionsUtil.java
@@ -48,6 +48,7 @@ import org.apache.pulsar.common.functions.Resources;
 import org.apache.pulsar.common.functions.Utils;
 import org.apache.pulsar.common.policies.data.ExceptionInformation;
 import org.apache.pulsar.common.policies.data.FunctionStatus;
+import org.apache.pulsar.common.util.ClassLoaderUtils;
 import org.apache.pulsar.common.util.RestException;
 import org.apache.pulsar.functions.proto.Function;
 import org.apache.pulsar.functions.proto.InstanceCommunication;
@@ -271,6 +272,7 @@ public class FunctionsUtil {
         Class<?>[] typeArgs = null;
         if (componentPackageFile != null) {
             typeArgs = extractTypeArgs(functionConfig, componentPackageFile, worker.getWorkerConfig().isForwardSourceMessageProperty());
+            componentPackageFile.delete();
         }
         if (StringUtils.isNotEmpty(functionConfig.getJar())) {
             V1alpha1FunctionSpecJava v1alpha1FunctionSpecJava = new V1alpha1FunctionSpecJava();
@@ -598,11 +600,18 @@ public class FunctionsUtil {
         if (componentPackageFile == null) {
             return null;
         }
-        ClassLoader clsLoader = FunctionConfigUtils.validate(functionConfig, componentPackageFile);
+        ClassLoader clsLoader = null;
+        try {
+            clsLoader = ClassLoaderUtils.extractClassLoader(componentPackageFile);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(
+                    String.format("Cannot extract class loader for package %s", componentPackageFile), e);
+        }
         if (functionConfig.getRuntime() == FunctionConfig.Runtime.JAVA && clsLoader != null) {
             try {
-                typeArgs = FunctionCommon.getFunctionTypes(functionConfig, clsLoader);
-            } catch (ClassNotFoundException | NoClassDefFoundError e) {
+                Class functionClass = ClassLoaderUtils.loadClass(functionConfig.getClassName(), clsLoader);
+                typeArgs = FunctionCommon.getFunctionTypes(functionClass, functionConfig.getWindowConfig() != null);
+            } catch (Exception e) {
                 throw new IllegalArgumentException(
                         String.format("Function class %s must be in class path", functionConfig.getClassName()), e);
             }
