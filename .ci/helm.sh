425ac4956b88a7efc49f16e74ce95aedca98f2c4
Marvin Cai <zxc@streamnative.io>
2021-12-20 22:57:39 +0800

    Debug.

--- .ci/helm.sh
+++ .ci/helm.sh
@@ -132,6 +132,7 @@ function ci::verify_function_mesh() {
         ${KUBECTL} describe pod -lname=${FUNCTION_NAME}
       fi
       WC=$(${KUBECTL} get pods -lname=${FUNCTION_NAME} --field-selector=status.phase=Running | wc -l)
+      ${KUBECTL} describe statefulset -lname=${FUNCTION_NAME}
     done
     ${KUBECTL} describe pod -lname=${FUNCTION_NAME}
     ${KUBECTL} logs -lname=${FUNCTION_NAME}  --all-containers=true
