3d34f8dd77090511cebcd88da9ff8932ebc7c82e
Yang Yang <yyang@streamnative.io>
2022-02-15 14:40:08 +0800

    Bump to 0.1.11-rc1

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ EOF
 
 main() {
   local local_kube="kind"
-  local fm_version="v0.1.9"
+  local fm_version="v0.1.11-rc1"
   local kind_name="kind"
   local kind_version="v0.7.0"
   local node_num=2
