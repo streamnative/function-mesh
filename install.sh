f528998b69ff467ca1d3de3d98de5903dea33d84
jiangpengcheng <scjiangpengcheng@gmail.com>
2022-11-10 01:55:36 +0000

    Update release rc version

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ EOF
 
 main() {
   local local_kube="kind"
-  local fm_version="v0.7.0"
+  local fm_version="v0.8.0-rc-1"
   local kind_name="kind"
   local kind_version="v0.7.0"
   local node_num=2
