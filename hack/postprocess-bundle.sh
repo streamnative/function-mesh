b896580d167405ad6a1c038e29ad1189b81c97ba
Rui Fu <rfu@apache.org>
2022-11-25 17:27:00 +0800

    validate openshift 0.8.0

--- hack/postprocess-bundle.sh
+++ hack/postprocess-bundle.sh
@@ -21,16 +21,21 @@
 # These labels are required to be in the bundle.Dockerfile, but can't be added by the operator-sdk automatically
 cat <<EOF >> bundle.Dockerfile
 # Certified Openshift required labels
-LABEL com.redhat.openshift.versions="v4.8"
+LABEL com.redhat.openshift.versions="v4.8-v4.11"
 LABEL com.redhat.delivery.operator.bundle=true
 LABEL com.redhat.delivery.backport=true
 LABEL operators.operatorframework.io.bundle.channel.default.v1="alpha"
 EOF
 
 # Add them to the bundle metadata also
-yq eval -i '.annotations."com.redhat.openshift.versions" = "v4.8"' bundle/metadata/annotations.yaml
+yq eval -i '.annotations."com.redhat.openshift.versions" = "v4.8-v4.11"' bundle/metadata/annotations.yaml
 yq eval -i '.annotations."com.redhat.delivery.operator.bundle" = true' bundle/metadata/annotations.yaml
 yq eval -i '.annotations."com.redhat.delivery.backport" = true' bundle/metadata/annotations.yaml
 yq eval -i '.annotations."operators.operatorframework.io.bundle.channel.default.v1" = "alpha"' bundle/metadata/annotations.yaml
 yq eval -i '.annotations."com.redhat.openshift.versions" headComment = "Certified Openshift required labels"' bundle/metadata/annotations.yaml
 
+# Add relatedImages
+yq -i '.spec.relatedImages = []' bundle/manifests/function-mesh.clusterserviceversion.yaml
+yq -i '.spec.relatedImages += {"name": "function-mesh", "image": ""}' bundle/manifests/function-mesh.clusterserviceversion.yaml
+yq -i '.spec.relatedImages += {"name": "kube-rbac-proxy", "image": "docker.cloudsmith.io/streamnative/mirrors/gcr.io/kubebuilder/kube-rbac-proxy@sha256:67ecb332573384515406ebd71816781366b70adb0eb66345e5980e92603373e1"}' bundle/manifests/function-mesh.clusterserviceversion.yaml
+yq -i '.spec.relatedImages[0].image += env(OPERATOR_IMG)' bundle/manifests/function-mesh.clusterserviceversion.yaml
