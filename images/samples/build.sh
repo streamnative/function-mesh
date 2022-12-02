3f72c1623133bef8937ff85652f374b578eb4b74
Rui Fu <rfu@apache.org>
2022-12-02 15:02:34 +0800

    add a connector sample

--- images/samples/build.sh
+++ images/samples/build.sh
@@ -21,25 +21,31 @@
 set -e
 
 PULSAR_IMAGE_TAG=${PULSAR_IMAGE_TAG:-"2.7.1"}
+PULSAR_IMAGE=${PULSAR_IMAGE:-"streamnative/pulsar-all"}
+JAVA_RUNNER_IMAGE=${RUNNER_IMAGE:-"streamnative/pulsar-functions-java-runner"}
 DOCKER_REPO=${DOCKER_REPO:-"streamnative"}
 JAVA_SAMPLE="pulsar-functions-java-sample"
 PYTHON_SAMPLE="pulsar-functions-python-sample"
 GO_SAMPLE="pulsar-functions-go-sample"
+CONNECTOR_ES_SAMPLE="pulsar-io-elasticsearch"
 KIND_PUSH=${KIND_PUSH:-false}
 CI_TEST=${CI_TEST:-false}
 
-
 echo "build java sample"
-docker build -t ${JAVA_SAMPLE} images/samples/java-function-samples --build-arg PULSAR_IMAGE_TAG="$PULSAR_IMAGE_TAG"
+docker build --platform linux/amd64 -t ${JAVA_SAMPLE} images/samples/java-function-samples --build-arg PULSAR_IMAGE_TAG="$PULSAR_IMAGE_TAG"
 docker tag ${JAVA_SAMPLE} "${DOCKER_REPO}"/${JAVA_SAMPLE}:"${PULSAR_IMAGE_TAG}"
 
 echo "build python sample"
-docker build -t ${PYTHON_SAMPLE} images/samples/python-function-samples --build-arg PULSAR_IMAGE_TAG="$PULSAR_IMAGE_TAG"
+docker build --platform linux/amd64 -t ${PYTHON_SAMPLE} images/samples/python-function-samples --build-arg PULSAR_IMAGE_TAG="$PULSAR_IMAGE_TAG"
 docker tag ${PYTHON_SAMPLE} "${DOCKER_REPO}"/${PYTHON_SAMPLE}:"${PULSAR_IMAGE_TAG}"
 
 echo "build go sample"
-docker build -t ${GO_SAMPLE} images/samples/go-function-samples --build-arg PULSAR_IMAGE_TAG="$PULSAR_IMAGE_TAG"
+docker build --platform linux/amd64 -t ${GO_SAMPLE} images/samples/go-function-samples --build-arg PULSAR_IMAGE_TAG="$PULSAR_IMAGE_TAG"
 docker tag ${GO_SAMPLE} "${DOCKER_REPO}"/${GO_SAMPLE}:"${PULSAR_IMAGE_TAG}"
+
+echo "build connector sample"
+docker build --platform linux/amd64 -t ${CONNECTOR_ES_SAMPLE} images/samples/pulsar-io-connector/pulsar-io-elasticsearch --build-arg PULSAR_IMAGE_TAG="$PULSAR_IMAGE_TAG" --build-arg PULSAR_IMAGE="$PULSAR_IMAGE" --build-arg RUNNER_IMAGE="$JAVA_RUNNER_IMAGE"
+docker tag ${GO_SAMPLE} "${DOCKER_REPO}"/${CONNECTOR_ES_SAMPLE}:"${PULSAR_IMAGE_TAG}"
 
 if [ "$KIND_PUSH" = true ] ; then
   echo "push images to kind"
