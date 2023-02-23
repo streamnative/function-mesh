e9bea0b82a27c616b810561da5e37007bd936bd1
jiang.pengcheng <scjiangpengcheng@gmail.com>
2023-02-23 12:02:54 +0800

    Only use SubPath when jar(py|go)Location is /pulsar/xxx.suffix

--- controllers/spec/common.go
+++ controllers/spec/common.go
@@ -1390,14 +1390,26 @@ func generateDownloaderVolumeMountsForRuntime(javaRuntime *v1alpha1.JavaRuntime,
 		subPath := getFilenameOfComponentPackage(downloadPath)
 		mountPath := downloadPath
 		// for relative path, volume should be mounted to the WorkDir
+		// and path also should be under the $WorkDir dir
 		if !strings.HasPrefix(downloadPath, "/") {
 			mountPath = WorkDir + downloadPath
+		} else if !strings.HasPrefix(downloadPath, WorkDir) {
+			mountPath = strings.Replace(downloadPath, "/", WorkDir, 1)
 		}
-		return []corev1.VolumeMount{{
-			Name:      DownloaderVolume,
-			MountPath: mountPath,
-			SubPath:   subPath,
-		}}
+		// if mount path is in the $WorkDir instead of its sub path
+		// we should use SubPath to avoid the mounted volume overwrite the $WorkDir
+		if !strings.Contains(strings.Replace(mountPath, WorkDir, "", 1), "/") {
+			return []corev1.VolumeMount{{
+				Name:      DownloaderVolume,
+				MountPath: mountPath,
+				SubPath:   subPath,
+			}}
+		} else {
+			return []corev1.VolumeMount{{
+				Name:      DownloaderVolume,
+				MountPath: mountPath[:len(mountPath)-len(subPath)],
+			}}
+		}
 	}
 	return nil
 }
