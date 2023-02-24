cdf87438d735dbde7c44527e145dca47247628f1
Neng Lu <nlu@streamnative.io>
2023-02-24 10:57:45 -0800

    fix typo

--- controllers/spec/common.go
+++ controllers/spec/common.go
@@ -1534,7 +1534,7 @@ func getSourceRunnerImage(spec *v1alpha1.SourceSpec) string {
 
 // getDefaultRunnerPodSecurityContext returns a default PodSecurityContext that runs as non-root
 func getDefaultRunnerPodSecurityContext(uid, gid int64, runAsNonRootEnv string) *corev1.PodSecurityContext {
-	nonRoot, err := strconv.ParseBool(nonAsNonRootEnv)
+	nonRoot, err := strconv.ParseBool(runAsNonRootEnv)
 	if err != nil {
 		nonRoot = true
 	}
