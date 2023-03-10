00179b3170d1d1836c5e8e11fb36efc2eca3c0e7
jiang.pengcheng <scjiangpengcheng@gmail.com>
2023-03-10 15:58:03 +0800

    Fix style

--- controllers/spec/common.go
+++ controllers/spec/common.go
@@ -22,7 +22,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"reflect"
 	"sort"
 	"strconv"
@@ -36,6 +35,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 const (
